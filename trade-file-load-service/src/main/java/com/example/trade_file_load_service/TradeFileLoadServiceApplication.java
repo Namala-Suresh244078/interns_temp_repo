3fa15d57714a3fa5124b0edf11d26f7492f8d36a
anirudha <anirudhabhat896@gmail.com>
2025-03-25 16:11:05 +0530

    US001 - intital commit

---
+++ trade-file-load-service/src/main/java/com/example/trade_file_load_service/TradeFileLoadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.trade_file_load_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeFileLoadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeFileLoadServiceApplication.class, args);
+	}
+
+}
