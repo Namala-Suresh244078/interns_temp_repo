5320a1dcd7717dc3ab1c995ef338eb4a5cd97eca
tejas <tejm910@gmail.com>
2025-03-27 08:51:39 +0530

    angular_structure_US-11

---
+++ ANGULAR_STRUCTURE/src/app/file-search/file-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-search',
+  imports: [],
+  templateUrl: './file-search.component.html',
+  styleUrl: './file-search.component.css'
+})
+export class FileSearchComponent {
+
+}
