48bed0b60f6ad6a20c115488f8ee43c63ffa3b9f
Varsha <hrvarsha1002@gmail.com>
2025-03-29 16:19:21 +0530

    Created Home Page Body

--- ANGULAR_STRUCTURE/src/app/pages/home/home.component.ts
+++ ANGULAR_STRUCTURE/src/app/pages/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  activeTab = 'home'; // Default active tab
 
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
 }
