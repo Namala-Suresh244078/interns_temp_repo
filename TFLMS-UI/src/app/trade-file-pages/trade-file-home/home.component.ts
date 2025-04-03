b8f5f4d3081de33aac96fadb8c09d25bb2c4d6d3
Varsha <hrvarsha1002@gmail.com>
2025-04-03 17:58:24 +0530

    Created a Home Page Body

--- TFLMS-UI/src/app/trade-file-pages/trade-file-home/home.component.ts
+++ TFLMS-UI/src/app/trade-file-pages/trade-file-home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  activeTab = 'home'; 
 
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
 }
