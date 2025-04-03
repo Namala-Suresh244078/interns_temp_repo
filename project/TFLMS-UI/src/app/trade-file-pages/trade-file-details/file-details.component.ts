2bc52ecc5ab9ffb13d21771c96d2f08316bbfbe8
tejas <tejm910@gmail.com>
2025-04-03 17:17:54 +0530

    footer updated

---
+++ project/TFLMS-UI/src/app/trade-file-pages/trade-file-details/file-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-details',
+  imports: [],
+  templateUrl: './file-details.component.html',
+  styleUrl: './file-details.component.css'
+})
+export class FileDetailsComponent {
+
+}
---
+++ project/TFLMS-UI/src/app/trade-file-status-update/status-update.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-update',
+  imports: [],
+  templateUrl: './status-update.component.html',
+  styleUrl: './status-update.component.css'
+})
+export class StatusUpdateComponent {
+
+}
