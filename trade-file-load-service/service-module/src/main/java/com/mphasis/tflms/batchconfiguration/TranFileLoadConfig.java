11df7a76ce8be767170b49e8baf5c5fa2733d8de
Joshwa <joshwaa822@gmail.com>
2025-04-03 17:44:50 +0530

    added item reader and batch constants in service module

---
+++ trade-file-load-service/service-module/src/main/java/com/mphasis/tflms/batchconfiguration/TranFileLoadConfig.java
@@ -0,0 +1,28 @@
+package com.mphasis.tflms.batchconfiguration;
+
+
+import com.mphasis.tflms.BatchConfigConstants;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import java.io.File;
+
+
+@Configuration
+public class TranFileLoadConfig extends BatchConfigConstants {
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<String[]> itemReader(@Value("#{jobParameters['file']}") String filePath) {
+        return new FlatFileItemReaderBuilder<String[]>()
+                .resource(new FileSystemResource(new File(filePath)))
+                .name(TRANS_FILE_READER_NAME)
+                .lineMapper((line, lineNumber) -> line.split(TRANS_FILE_DELIMITER))
+                .strict(false)
+                .build();
+    }
+}
