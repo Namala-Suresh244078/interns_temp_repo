2340dd5cb366ee19884f08e4cbc12d38af0c9748
Rdeekshitha123 <deekshithar1307@gmail.com>
2025-04-04 11:59:01 +0530

    Created the AbstractBaseEntity, ProcessedFile and File_validation classes

--- trade-file-load-service/entity-module/src/main/java/com/mphasis/tflms/AbstractBaseEntity.java
+++ trade-file-load-service/entity-module/src/main/java/com/mphasis/tflms/AbstractBaseEntity.java
@@ -1,36 +1,34 @@
 package com.mphasis.tflms;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.io.Serializable;
-import com.mphasis.tflms.constants.AbstractBaseEntityConstants;
+
+import static com.mphasis.tflms.constants.AbstractBaseEntityConstants.*;
 
 @Getter
 @Setter
 @MappedSuperclass
 public abstract class AbstractBaseEntity implements Serializable {
 
-
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = AbstractBaseEntityConstants.COLUMN_ID)
+    @Column(name = COLUMN_ID)
     private Long id;
 
-    @Column(name = AbstractBaseEntityConstants.COLUMN_CREATED_BY, nullable = false, updatable = false)
+    @Column(name = COLUMN_CREATED_BY, nullable = false, updatable = false)
     private String createdBy;
 
-    @Column(name = AbstractBaseEntityConstants.COLUMN_CREATED_DATE, nullable = false, updatable = false)
+    @Column(name = COLUMN_CREATED_DATE, nullable = false, updatable = false)
     private LocalDateTime createdDate;
 
-    @Column(name = AbstractBaseEntityConstants.COLUMN_LAST_MODIFIED_BY, nullable = false)
+    @Column(name = COLUMN_LAST_MODIFIED_BY, nullable = false)
     private String lastModifiedBy;
 
-    @Column(name = AbstractBaseEntityConstants.COLUMN_LAST_MODIFIED_DATE, nullable = false)
+    @Column(name = COLUMN_LAST_MODIFIED_DATE, nullable = false)
     private LocalDateTime lastModifiedDate;
 }
