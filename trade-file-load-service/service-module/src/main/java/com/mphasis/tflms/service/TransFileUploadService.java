ef7d946c9ef221e6832df44f9e80d95222067d85
B S Bhargav <b1657sb@gmail.com>
2025-04-05 00:51:09 +0530

    Update TransFileUploadService.java

--- trade-file-load-service/service-module/src/main/java/com/mphasis/tflms/service/TransFileUploadService.java
+++ trade-file-load-service/service-module/src/main/java/com/mphasis/tflms/service/TransFileUploadService.java
@@ -1,7 +1,8 @@
 package com.mphasis.tflms.service;
 
 import org.springframework.web.multipart.MultipartFile;
+import java.util.concurrent.CompletableFuture;
 
 public interface TransFileUploadService {
-    void runJob(MultipartFile multipartFile) throws Exception;
+    CompletableFuture<Void> runJob(MultipartFile multipartFile) throws Exception;
 }
