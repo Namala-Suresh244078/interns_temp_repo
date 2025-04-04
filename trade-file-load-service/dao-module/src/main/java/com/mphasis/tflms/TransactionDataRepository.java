95ea6305d4a70959eaa1e0b3c6d7bea1698ac242
SoujanyaN <soujanyanaidu28@gmail.com>
2025-04-04 21:21:18 +0530

    Created a transaction data repository

---
+++ trade-file-load-service/dao-module/src/main/java/com/mphasis/tflms/TransactionDataRepository.java
@@ -0,0 +1,6 @@
+package com.mphasis.tflms;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionDataRepository extends JpaRepository<TransactionDataRepository, Long>{
+}
