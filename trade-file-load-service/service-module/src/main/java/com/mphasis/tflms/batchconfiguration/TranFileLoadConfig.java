b939be06c1c53f6a09f9833350b4fee6d723bd4d
anirudha <anirudhabhat896@gmail.com>
2025-04-05 10:26:40 +0530

    US-18-Job and Step Configuration

--- trade-file-load-service/service-module/src/main/java/com/mphasis/tflms/batchconfiguration/TranFileLoadConfig.java
+++ trade-file-load-service/service-module/src/main/java/com/mphasis/tflms/batchconfiguration/TranFileLoadConfig.java
@@ -2,13 +2,20 @@ package com.mphasis.tflms.batchconfiguration;
 
 
 import com.mphasis.tflms.BatchConfigConstants;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
 import java.io.File;
 
 
@@ -23,6 +30,31 @@ public class TranFileLoadConfig extends BatchConfigConstants {
                 .name(TRANS_FILE_READER_NAME)
                 .lineMapper((line, lineNumber) -> line.split(TRANS_FILE_DELIMITER))
                 .strict(false)
+                .build();
+    }
+    @Bean
+    public TransFileWriter itemWriter(){
+        return new TransFileWriter();
+    }
+    @Bean
+    public TransactionDataProcessor itemProcessor(){
+
+        return new TransactionDataProcessor();
+        }
+    @Bean
+    public Job job (JobRepository jobRepository, Step step) {
+        return new JobBuilder("tradeFileLoadJob", jobRepository)
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public Step step (JobRepository jobRepository, PlatformTransactionManager transactionManager, FlatFileItemReader<String[]> itemReader) {
+        return new StepBuilder("tradeFileProcessingStep", jobRepository)
+                .<String[], TransDataValidationWrapper>chunk(CHUNK_SIZE, transactionManager)
+                .reader(itemReader)
+                .processor(itemProcessor())
+                .writer(itemWriter())
                 .build();
     }
 }
